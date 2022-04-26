4c465ceb13a9345c2cca2f2ec43558e1de8f813e
bamm99 <benjamin.mosso.1701@alumnos.ubiobio.cl>
2022-04-25 21:10:18 -0400

    agregado de funcionalidad extra

--- main.c
+++ main.c
@@ -9,6 +9,12 @@
 //definimos tamaño de arreglos
 #define sizecmd 1024
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 int main() {
     //declaracion de variables
     char command[sizecmd];
@@ -30,21 +36,20 @@ int main() {
                      "    .::!55!::J5Y::~557.:?55~::::::B@~::.\n"
                      "    .::!55!::J5Y::~557.:?55~::::::B@~::.\n"
                      "    .::~55!::J5Y::~557.:?55^::::::B@~::.\n"
-                     "    .::~55!::J5Y::~557.:?55^:::::.B@~::.\n"
-                     "    .::^55!::J5Y::~557.:?5Y^:^^^^J@B:::.\n"
-                     "    .::^55!::JPY::~557.:?PY:~JJJPBJ^::. \n"
                      "    .:::JY~.:?Y?:.^JJ!..7J?:::::::.:..  \n"
                      "    .::~!!!!!!!!!?7??777???~:::...      \n"
                      "    :::YPPPPPPPPG@@@@@@@@@@G:::         \n"
                      "    :::~~~~~~~~~~!!!!!!!!!!~:::         \n"
-                     "    ...........................\n\n"
+                     "    ...........................\n"
                      "      ========SHELL-ITA======== \n"
+                     "   Benjamin Mosso and Felipe Rodriguez  \n \n"
                      "\033[0m";
     printf("%s", bienvenida);
     printf("Escribe 'Salir' para salir.\n\n");
 
 
     do {
+        
         printf("shell-ita $~ ");
         fgets(command, sizecmd, stdin); //ingreso de llamadas al sistema
         command[strlen(command) - 1] = '\0';
@@ -63,7 +68,13 @@ int main() {
     if (strcmp (command, "createdir") == 0 ) //validacion de llamada al sistema
        {
         mkdir(tokens[1], 0755);         //llamada mkdir alias createdir
-        printf("%i \n", errno);
+        //printf("%i \n", errno);
+
+        if (errno == ENOENT){
+            printf ("El enlace nombrado por oldpath no existe; o, un directorio el componente en newpath no existe; o, viejo camino o newpath es una cadena vacía. \n");
+        }else if (errno == EFAULT){
+            printf ("oldpath o newpath puntos fuera de su dirección accesible espacio. \n");
+        } 
     }
 
     else if (strcmp (command, "listar")== 0) //validacion de llamada al sistema
@@ -78,27 +89,94 @@ int main() {
         } while ((entr = readdir(directorio)) != NULL);
         
         printf("\n");
-        printf("%i \n", errno);
+        //printf("%i \n", errno);
     }
     else if (strcmp (command, "changename")== 0 ) //validacion de llamada al sistema
     {
     
         rename(tokens[1], tokens[2]);
         if (errno == ENOENT){
-            printf ("El enlace nombrado por oldpath no existe; o, un directorio el componente en newpath no existe; o, viejo camino o newpath es una cadena vacía.");
+            printf ("El enlace nombrado por oldpath no existe; o, un directorio el componente en newpath no existe; o, viejo camino o newpath es una cadena vacía. \n");
         }else if (errno == EFAULT){
-            printf ("oldpath o newpath puntos fuera de su dirección accesible espacio.");
+            printf ("oldpath o newpath puntos fuera de su dirección accesible espacio. \n");
         }
     }
-    else if (strcmp (command, "Salir")== 0) //validacion de salida
+    else if(strcmp (command, "color")==0) // funcionalidad extra, cambio de color
+    {
+        int color=0;
+        
+        if (strcmp (tokens[1], "rojo")== 0)
+        {
+            color=1;
+
+        }else if ((strcmp (tokens[1], "verde")== 0))
+        {
+            color=2;
+        }else if (strcmp (tokens[1], "azul")== 0)
+        {
+            color=3;
+        }else if (strcmp (tokens[1], "default")== 0)
+        {
+            color=4;
+        }
+        printf("%d \n", color);
+
+        switch (color)
+        {
+        case 1:
+            printf( ANSI_COLOR_RED "el texto es color ROJO!\n");
+            break;
+        case 2:
+            printf( ANSI_COLOR_GREEN "el texto es color VERDE!\n");
+            break;
+        case 3:
+            printf( ANSI_COLOR_BLUE "El texto es color AZUL!\n");
+            break;
+        case 4:
+            printf( ANSI_COLOR_RESET "el texto vuelve a sus colores default!\n");
+            break;
+        default:
+            break;
+        }
+    } 
+    else if(strcmp (command, "Salir")== 0) //validacion de salida
     {
         printf("\n");
         
-    }else {
+    }else{
         printf("ingrese una llamada al sistema valida \n"); //mensaje de error en caso de que no se ingrese algo congruente
         }
 
     } while ( strcmp(command, "Salir") !=0 );
+
+    char *despedida  = "\033[92m"
+                     "\n"
+                     "      ........................         \n"
+                     "    ............................        \n"
+                     "    .:^~!!!!!!!!!!!!!!!!!!!~^:.::.      \n"
+                     "    :::J555555555Y555555555?.:::.....   \n"
+                     "    :::J55?^~Y5Y~^755?^^J55?.!J?55!:::. \n"
+                     "    :::J55!  J5Y:  557  ?557.^~~7P@P::: \n"
+                     "    :::?55!  J5Y:  557  ?557::::.:B@~::.\n"
+                     "    :::755!  J5Y:  557  ?55!::::::B@~::.\n"
+                     "    .::755!  J5Y:  557  ?55!::::::B@~::.\n"
+                     "    .::!55!  J5Y:  557  ?55~::::::B@~::.\n"
+                     "    .::!55!  J5Y:  557  ?55~::::::B@~::.\n"
+                     "    .::~55!  J5Y:  557  ?55^::::::B@~::.\n"
+                     "    .::~55!  J5Y:  557  ?55^:::::.B@~::.\n"
+                     "    .::^55!  J5Y:  557  ?5Y^:^^^^J@B:::.\n"
+                     "    .::^55!  JPY:  557  PY:~JJJPBJ^::. \n"
+                     "    .:::JY~..?Y?:.^JJ!..7J?:::::::.:..  \n"
+                     "    .::~!!!!!!!!!?7??777???~:::...      \n"
+                     "    :::YPPPPPPPPG@@@@@@@@@@G:::         \n"
+                     "    :::~~~~~~~~~~!!!!!!!!!!~:::         \n"
+                     "    ...........................\n"
+                     "      ========SHELL-ITA======== \n"
+                     "   Benjamin Mosso and Felipe Rodriguez  \n \n"
+                     "\033[0m";
+
+                     printf("Se termino la Shell-ita \n %s",despedida);
+
 
     return 0;
 }
